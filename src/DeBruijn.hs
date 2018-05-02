365f321e9f1e4ec11ed25b84dad0e119e3abae16
Paul Laffitte <paul.laffitte@epitech.eu>
2018-05-03 00:53:48 +0200

    improvements

--- src/DeBruijn.hs
+++ src/DeBruijn.hs
@@ -18,34 +18,35 @@ mergeContext :: ([a], [a]) -> ([a], [a]) -> ([a], [a])
 mergeContext a b = (fst a ++ fst b, snd b)
 
 deBruijn :: Int -> String -> IO String
-deBruijn order alphabet = do
-    print context
-    return [ alphabet !! x | x <- bruijn]
+deBruijn order alphabet = return [ alphabet !! x | x <- bruijn]
     where
         base    = length alphabet
-        bruijn  = snd context
+        bruijn  = fst context
         context = db order base 1 1 ([], replicate (order * base) 0)
 
 db :: Int -> Int -> Int -> Int -> Context -> Context
 db order base t p context
-    | t <= order        = mergeContext dbRet genRet
-    | mod order p == 0  = (bruijn ++ take t a, a)
+    | t <= order        = dbGen order base t p letter context
+    | mod order p == 0  = (bruijn ++ take p a, a)
     | otherwise         = (bruijn, a)
         where
             letter  = a !! (t - p)
             bruijn  = fst context
             a       = snd context
-            a'      = updateElem t letter a
-            dbRet   = db order base (t + 1) p (bruijn, a')
-            genRet  = generate order base t (letter + 1) (mergeContext (bruijn, a') dbRet)
 
 generate :: Int -> Int -> Int -> Int -> Context -> Context
 generate order base t letter context
-    | letter < base = mergeContext dbRet genRet
-    | otherwise     = ([], [])
+    | letter < base = dbGen order base t t letter context
+    | otherwise     = (bruijn, a)
+        where
+            bruijn  = fst context
+            a       = snd context
+
+dbGen :: Int -> Int -> Int -> Int -> Int -> Context -> Context
+dbGen order base t p letter context = mergeContext dbRet genRet
     where
         bruijn      = fst context
         a           = snd context
         a'          = updateElem t letter a
-        dbRet       = db order base (t + 1) t (bruijn, a')
+        dbRet       = db order base (t + 1) p (bruijn, a')
         genRet      = generate order base t (letter + 1) (mergeContext (bruijn, a') dbRet)
