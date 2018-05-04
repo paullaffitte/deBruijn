f7ae98a3357a2f062d6bedc9a0a23175fafd41d7
Paul Laffitte <paul.laffitte@epitech.eu>
2018-05-04 23:26:38 +0200

    getLines (until "END" user input)

--- src/Flags.hs
+++ src/Flags.hs
@@ -27,7 +27,21 @@ unique order alphabet = do
     lseq <- getLine
     rseq <- getLine
     printOKKO (checkSequence lseq && checkSequence rseq && (not $ areEquivalents lseq rseq))
-        where checkSequence = isDeBruijnSequence order alphabet
+    where checkSequence = isDeBruijnSequence order alphabet
+
+getLines :: IO [String]
+getLines = do lines <- readLines []; return $ reverse lines
+    where
+        readLines :: [String] -> IO [String]
+        readLines xs = do
+            x <- getLine
+            checkLine (x : xs)
+        checkLine :: [String] -> IO [String]
+        checkLine xs
+            | head xs == "END"  = return $ tail xs
+            | otherwise         = readLines xs
 
 clean :: Int -> String -> IO ()
-clean order alphabet = putStrLn "clean"
+clean order alphabet = do
+    lines <- getLines
+    putStrLn $ show lines
